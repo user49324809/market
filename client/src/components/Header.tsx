5803536524532f4b74564cf56ac856a8a80962b7
Natalia <inna6903zaharova@yandex.ru>
2026-09-24 15:07:07 +0300

    Fix pay

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,26 @@
 import { ACTIVE_VILLAGER_ID } from '../config/player';
 import { BalanceWidget } from './BalanceWidget';
 
-export function Header() {
+interface HeaderProps {
+  balanceRefreshKey?: number;
+}
+
+export function Header({
+  balanceRefreshKey = 0,
+}: HeaderProps) {
   return (
     <header className="app-header">
       <div className="app-header__brand">
-        <span className="app-header__eyebrow">Управление поселением</span>
+        <span className="app-header__eyebrow">
+          Управление поселением
+        </span>
         <h1>Mini ERP Market</h1>
       </div>
-      <BalanceWidget villagerId={ACTIVE_VILLAGER_ID} />
+
+      <BalanceWidget
+        villagerId={ACTIVE_VILLAGER_ID}
+        refreshKey={balanceRefreshKey}
+      />
     </header>
   );
 }
